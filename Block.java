01677df927f51ee1aebcecc52b89dac7516073f6
Josh <flinkeydink@gmail.com>
2025-05-12 11:46:30 -0400

    isChainValid update. Will ensure mining is done regarding to hash layer.

--- Block.java
+++ Block.java
@@ -6,22 +6,30 @@ public class Block {
     public String previousHash;
     private String data;
     private long timeStamp;
+    private int nonce;
 
     // Block constructor
     public Block(String data, String previousHash) {
         this.data = data;
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
-        this.hash = CalculateHash();
+        this.hash = calculateHash();
     }
 
-    public String CalculateHash() {
+    public String calculateHash() {
         String calculatedhash = StringUtil.applySha256(
-                previousHash +
-                        Long.toString(timeStamp) +
-                        data);
+                previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + data);
         return calculatedhash;
     }
+    	public void mineBlock(int difficulty) {
+		String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0" 
+		while(!hash.substring( 0, difficulty).equals(target)) {
+			nonce++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!! : " + hash);
+	}
+
 
     // Override toString to provide a proper string representation to output
     @Override
@@ -29,4 +37,6 @@ public class Block {
         return "Block[data=" + data + ", hash=" + hash + ", previousHash=" + previousHash + ", timeStamp=" + timeStamp
                 + "]";
     }
+
 }
+
